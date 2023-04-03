7df4b1f52ccfdfd2e4b21780f3d3d203a1f9ac43
AusSS7 <riolesteban05@gmail.com>
2023-04-02 22:04:39 -0700

    Task 0. Positive anything is better that negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - entry point
+ * return: alwasy 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
