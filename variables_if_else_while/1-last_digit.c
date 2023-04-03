ffb12799b9ff1c4907a30e71d54cf307912f9da6
AusSS7 <riolesteban05@gmail.com>
2023-04-03 00:07:30 -0700

    Task 1. The last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of function main
+ * Description: 
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	  {
+	    last_digit = n % 10
+	      printf("Last digit of %d is %d\n", n,last_digit);
+	  }
+	else if (n == 0)
+	  {
+	    last_digit = n % 10
+	      printf("Last digit of %d is %d", n,last_digit);
+	  }
+	else if (n < 0)
+	  {
+	    last_digit = n % 10
+	    printf("Last digit of %d is -%d", n,last_digit);
+	  }
+	return (0);
+}
