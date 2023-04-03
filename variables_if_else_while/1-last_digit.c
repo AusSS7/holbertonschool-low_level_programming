13b6b96acbee785e732035cbecf782a4fab5855b
AusSS7 <riolesteban05@gmail.com>
2023-04-03 00:09:03 -0700

    Task 1. The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,17 +14,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	  {
-	    last_digit = n % 10
+	    last_digit = n % 10;
 	      printf("Last digit of %d is %d\n", n,last_digit);
 	  }
 	else if (n == 0)
 	  {
-	    last_digit = n % 10
+	    last_digit = n % 10;
 	      printf("Last digit of %d is %d", n,last_digit);
 	  }
 	else if (n < 0)
 	  {
-	    last_digit = n % 10
+	    last_digit = n % 10;
 	    printf("Last digit of %d is -%d", n,last_digit);
 	  }
 	return (0);
