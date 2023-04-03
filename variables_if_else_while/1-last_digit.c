17fb28e0043545fe8453e2ee59966c438dc7ac40
AusSS7 <riolesteban05@gmail.com>
2023-04-03 00:31:04 -0700

    Task 1. The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	    last_digit = n % 10;
 	    if (last_digit == 0)
 	      {
-		printf("Last digit of %d is %d and  is 0\n", n,last_digit);
+		printf("Last digit of %d is %d and is 0\n", n,last_digit);
 	      }
 	  }
 	else if (n < 0)
@@ -34,7 +34,7 @@ int main(void)
 	    last_digit = n % 10;
 	    if (last_digit <= 5 )
 	      {
-		printf("Last digit of %d is -%d and is less than 6 and not 0\n", n,last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,last_digit);
 	      }
 	  }
 	return (0);
