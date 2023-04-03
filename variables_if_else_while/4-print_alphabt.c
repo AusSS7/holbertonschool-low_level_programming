560322131987c2ce520807aa6d0cf745641caf04
AusSS7 <riolesteban05@gmail.com>
2023-04-03 05:53:07 -0700

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - It prints alphabet in lowercase except q and e
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char letter_lower;
+
+ while (letter_lower <= 'z')
+   {
+   if (letter_lower == 'e')
+     {
+       continue;
+       //putchar(letter_lower);
+     }
+   putchar(letter_lower);
+   letter_lower++;
+   }
+ putchar('\n');
+ return (0);
+}
