964910c1a46ba164b26efe42eccefb8e98b08606
AusSS7 <riolesteban05@gmail.com>
2023-04-09 01:16:01 -0700

    Task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 int i;
 char letter;
 
-for (i = 0; i < 11; i++)
+for (i = 0; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
