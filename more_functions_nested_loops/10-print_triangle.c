32f2f80dd1eb060650320a90ae0989659f984c77
AusSS7 <riolesteban05@gmail.com>
2023-04-16 07:10:59 -0700

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* print_triangle - prints triangle
+* @size: size of the triangle
+* Return: None
+*/
+
+void print_triangle(int size)
+{
+
+int x, y, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+b = (size - x) - 1;
+if (y < b)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}    
+}
+
