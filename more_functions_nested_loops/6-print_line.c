e53b767b9076b703f9724b2455b09c8313568a8f
AusSS7 <riolesteban05@gmail.com>
2023-04-16 05:04:31 -0700

    Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,13 @@
 
 void print_line(int n)
 {
-for (; n <= 10; n++)
-{
-if (n <= 0)
-{
-_putchar('\n');
-}
- else if (n > 0)
+
+if (n > 0)
 {
 _putchar('_');
 }
-}
+else if (n <= 0)
+{
 _putchar('\n');
+}
 }
