600712eb32e9b2c33202d338ff8c12c30f584659
AusSS7 <riolesteban05@gmail.com>
2023-04-16 06:39:30 -0700

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,34 @@
+#include "main.h"
 #include <stdio.h>
 
-int main() {
+/**
+* main - Print 1 to 100
+*
+* Return: Always 0
+*/
+int main(void)
+{
 int x;
 
-for (x = 1; x < 101; x ++)
+for (x = 1; x < 101; x++)
 {
-if ((x%3)==0)
+if (x % 3 == 0 && x % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (x % 3 == 0)
 {
 printf("Fizz ");
 }
-else if ((x%5)==0)
+else if (x % 5 == 0)
 {
-printf("Buzz ");   
-}
-else if (((x%3)==0) && ((x%5)==0))
-{
-printf("FizzBuzz ");
+printf("Buzz ");
 }
 else
 {
 printf("%d ", x);
 }
 }
+printf("\n");
 return (0);
 }
