e04300ea1562c56aa9b441dfbfa308c5f1b2158b
AusSS7 <riolesteban05@gmail.com>
2023-04-18 06:36:02 -0700

    Task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 {
 int a, b, s;
 s = strlen(str);
-a = (s / 2) + 1;
+a = (s - 1) / 2;
 
 for (b = 0; b <= s; b++)
 {
@@ -21,5 +21,5 @@ if (b >= a)
 printf("%c", str[b]);
 
 }
-
+printf("\n");
 }
