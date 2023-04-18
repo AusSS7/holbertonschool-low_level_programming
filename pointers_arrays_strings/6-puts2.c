76f81bda6335e1c487b8cfbb3ee2c40dea781c00
AusSS7 <riolesteban05@gmail.com>
2023-04-18 05:53:58 -0700

    Task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,23 +3,20 @@
 #include <string.h>
 
 /**
-*puts2 - 
-*@str:
+*puts2 - prints every other character of a string
+*@str: value of the string
 *Return: None
 */
 
 void puts2(char *str)
 {
-  int a;
-  for (a = 0; str[a] != '\0'; a++)
-    {
-
-  if (a % 2 == 0)
-    {
-            
+int a;
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+{
 printf("%c", str[a]);
-
- }
-    }
+}
+}
 printf("\n");
 }
