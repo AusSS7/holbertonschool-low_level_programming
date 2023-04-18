93514f5f8079387bc299b880c4036d5e0771ff0e
AusSS7 <riolesteban05@gmail.com>
2023-04-17 21:56:13 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: character to be executed
+*Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+int a;
+
+for (a = 0; s[a] != '\0'; a++)
+{
+}
+return (0);
+}
