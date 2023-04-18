4c6e9f36b4c79039b3733863daed3642c7a6e676
AusSS7 <riolesteban05@gmail.com>
2023-04-18 05:20:38 -0700

    Task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*puts2 - 
+*@str:
+*Return: None
+*/
+
+void puts2(char *str)
+{
+  int a;
+  for (a = 0; str[a] != '\0'; a++)
+    {
+      if (str[a] % 2 == 0)
+	{
+	  printf("%d", a);
+	}
+    }
+  printf("\n");
+}
