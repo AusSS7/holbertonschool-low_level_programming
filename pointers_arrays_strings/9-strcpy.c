9d69bf3932f5453efd00b599068dfc3f8260e48b
AusSS7 <riolesteban05@gmail.com>
2023-04-18 21:39:13 -0700

    Task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int a, len;
+  int a;
 
-len = strlen(src);
-for (a = 0; a < len &&  src[a] != '\0'; a++)
+
+for (a = 0; src[a] != '\0'; a++)
 dest[a] = src[a];
-for (; a < len; a++)
-dest[a] = '\0';
 
 return (dest);
 }
