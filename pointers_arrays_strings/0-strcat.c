a89ac5c69057fb8bf579119d9f813eb7b1922dae
AusSS7 <riolesteban05@gmail.com>
2023-04-21 00:15:38 -0700

    Task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* *_strcat - concatenates two strings
+*@dest:
+*@src:
+*Return: dest value
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int a, dest_len;
+
+	dest_len = strlen(dest);
+	for (a = 0; src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+       	dest[dest_len + a] = '\0';
+
+	return (dest);
+}
