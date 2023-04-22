59411100013179eeb646973e36226b79dd4a2d39
AusSS7 <riolesteban05@gmail.com>
2023-04-21 22:16:19 -0700

    Task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,37 +2,26 @@
 #include <string.h>
 
 /**
- * *leet - encodes a string into 1337.
- *
- * Return:
- */
+* *leet - encodes a string into 1337.
+* @s: pointer variable
+* Return: r
+*/
 
 char *leet(char *s)
-	{
-  int a;
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
 
-  for (a = 0; s[a] != '\0'; a++)
-  {
-      while (s[a] == 65 || s[a] == 97)
-      {
-          s[a] = 52;
-      }
-      while (s[a] == 69 || s[a] == 101)
-      {
-          s[a] = 51;
-      }
-      while (s[a] == 79 || s[a] == 111)
-      {
-          s[a] = 48;
-      }
-      while (s[a] == 84 || s[a] == 116)
-      {
-          s[a] = 55;
-      }
-      while (s[a] == 76 || s[a] == 108)
-      {
-          s[a] = 49;
-      }
-  }
-  return (s);
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
 }
