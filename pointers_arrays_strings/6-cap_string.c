4321f85404ffc1b5c39d3c54b9b6c5167ba55c4f
AusSS7 <riolesteban05@gmail.com>
2023-04-21 22:33:56 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,28 +9,24 @@
 
 char *cap_string(char *str)
 {
+	char *r = str;
 	int i;
+	int b;
+	char a[] = " \t\n,.!?\"(){}";
+	int cap;
 
+	cap = 1;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[0] >= 97 && str[0] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		cap = 0;
+		for (b = 0; b < 12; b++)
 		{
-			str[0] = str[0] - 32;
+			if (str[b] == a[i])
+				cap = 1;
 		}
-		else if (str[i - 1] == ' ')
-		{
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-			}
-		}
-		else if (str[i - 1] >= 9 && str[i - 1] <= 10)
-		{
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-			}
-		}
+		str++;
 	}
-	return (str);
+	return (r);
 }
