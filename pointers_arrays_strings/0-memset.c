83debb412786798fe504cdbed256417521e5e4b6
AusSS7 <riolesteban05@gmail.com>
2023-04-23 00:03:47 -0700

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* *_memset - fills memory with a concstant byte
+* @s: pointer to the object or block of memory that needs to be set
+* @b: value that is copied, converted to unsigned int n
+* @n: number of bytes to be set
+* Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s++ = (unsigned char)b;
+	}
+	return (s);
+}
