fd2a2ec9eb830642183878897794f8d4fe4ee47e
AusSS7 <riolesteban05@gmail.com>
2023-04-23 06:25:23 -0700

    Task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+/**
+* int_strspn - gets the length of a prefix substring
+* @s: This is the main C string to be scanned.
+* @accept: This is the string containing the list of characters to match in str1.
+* Return:
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+
+	if ((s == NULL) || (accept == NULL))
+		return (a);
+	while(*s && strchr(accept,*s++))
+	{
+		a++;
+	}
+
+	return a;
+}
