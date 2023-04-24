f32a00945e0c2e8d924ff3f9b0f1efd992dabb51
AusSS7 <riolesteban05@gmail.com>
2023-04-23 21:42:30 -0700

    task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
 * _strspn - gets the length of a prefix substring
 * @s: This is the main C string to be scanned.
-* @accept: This is the string containing the list of characters to match in str1
+* @accept: This is the string containing the list of characters to match
 * Return: the number of bytes in the initial segment of s
 */
 unsigned int _strspn(char *s, char *accept)
