26d253fe9d060d376dfae6db46559087466a14a9
AusSS7 <riolesteban05@gmail.com>
2023-04-23 22:43:32 -0700

    Task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
 #include <string.h>
 
 /**
-* *_strpbrk -
-* s − This is the C string to be scanned.
-* accept − This is the C string containing the characters to match.
-* Return:
+* *_strpbrk - searches a string for any of a set of bytes.
+* @s: This is the C string to be scanned.
+* @accept: This is the C string containing the characters to match.
+* Return: Pointer to the first character in s
 */
 
 char *_strpbrk(char *s, char *accept)
@@ -15,14 +15,13 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (a = 0; accept[a]; a++)
 		{
-		       	/* printf("%c", accept[a]);*/
+/* printf("%c", accept[a]);*/
 			if (*s == accept[a])
-			{
+
 				/*printf("\n%s\n", s); */
 				return (s);
-			}
-			s++;
 		}
+		s++;
 	}
 	return (NULL);
 }
