a6e55fadc4dd86189c807c78b176af4cecc36373
AusSS7 <riolesteban05@gmail.com>
2023-05-20 18:01:32 -0700

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int argc, char* argv[])
+/**
+* main - prints its name, followed by a new line
+* @argc: strings containing the program name and arguments
+* @argv: number of strings in argv
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
